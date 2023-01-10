382e865547c764a010a8f0b4ebdae44b0bb39c57
Geethika-30 <112071177+Geethika-30@users.noreply.github.com>
2023-01-10 22:33:53 +0530

    Add files via upload

---
+++ ceaser cipher.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#unclude<string.h>
+#include<conio.h>
+#include<ctype.h>
+void main()
+{
+	char plain[10],cipher[10];
+	int key,i,length;
+	int result;
+	clrscr();
+	printf("\n enter the plain text:");
+	scanf("%s",plain);
+	printf("\n enter the key value:");
+	scanf("%d",&key);
+	printf("\n \n \t plain text: %s",plain);
+	printf("\n \n \n encrypted text:");
+	for(i=o,length=strlen(plain);i<length;i++)
+	{
+		
+	}
+}
